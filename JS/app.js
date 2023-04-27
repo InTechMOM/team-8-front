dbe75299fbf6a1c7e63f064ffcd49e2af9b34e87
Amairani Arellano <lyliarellano@gmail.com>
2023-04-26 21:59:42 -0600

    prueba api fake 1

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 const correo = document.getElementById('emailInput')
 const ingreso = document.getElementById('ingresar')
-const vercomo = document.getElementsByName('persona')
+const vercomo = document.getElementsByName('rol')
 
 ingreso.addEventListener('click', (e) => {
     e.preventDefault()
@@ -9,11 +9,6 @@ ingreso.addEventListener('click', (e) => {
         vercomo: vercomo.value
     }
 })
-function todas() { //llamar a todas las funciones
-    guardar()
-    rol()
-}
-
 
 function guardar(){ //funcion para guardar y validar formulario
     var email=document.getElementById("emailInput").value;
@@ -21,23 +16,21 @@ function guardar(){ //funcion para guardar y validar formulario
         alert ("Ingresar E-mail");
         document.getElementById("emailInput").focus();
     } else{
-        console.log(email);
-        document.getElementById("emailInput").value="";
         if (document.getElementById ('estudiante').checked) {
+            console.log(email);
+            document.getElementById("emailInput").value="";
             console.log ("estudiante")
             window.location.href = "pages/perfil-alumno.html";
+        } else{
+            if (document.getElementById ('profesor').checked) {
+                console.log(email);
+                document.getElementById("emailInput").value="";
+                console.log ("profesor")
+                window.location.href = "pages/perfil-profesor.html";
+            }
+            else{
+                alert ("Seleccione Profesor o Estudiante");
+            }
         }
-        if (document.getElementById ('profesor').checked) {
-            console.log ("profesor")
-            window.location.href = "pages/perfil-profesor.html";
-        }/*
-        else{
-            alert ("Seleccione Profesor o Estudiante");
-        }*/
-    } 
-    
+    }    
 }
-
-/*function rol() { //valor y redireccion de rol
-
-}*/
