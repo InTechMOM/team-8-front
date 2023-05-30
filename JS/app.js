cfd8f5efabf38c0a4b93435e034cb35e1801fff2
Amairani Arellano <lyliarellano@gmail.com>
2023-05-29 18:39:20 -0600

    correcciones finales 2

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
+//validaciones
+
 const correo = document.getElementById('emailInput')
 const ingreso = document.getElementById('ingresar')
 const vercomo = document.getElementsByName('rol')
@@ -34,3 +36,25 @@ function guardar(){ //funcion para guardar y validar formulario
         }
     }    
 }
+
+//Conexion Api Post
+
+
+const form = document.getElementById('datos');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const data = new FormData(event.target);
+
+    const dataObject = Object.fromEntries (data)
+
+    fetch('https://team-8-back.onrender.com/users', { 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data0bject)
+    })
+    .then((response) => response.json())
+    .then((data) => )
+})
